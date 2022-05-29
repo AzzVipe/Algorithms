8f3a41e1183a0a0178b2c2bb426e71e66580c5ec
Pritam1812 <pritamhore2001@gmail.com>
2022-05-30 01:17:19 +0530

    Pritam: Designing Menu

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void main()
 {
-	printf("Final Testing Commit!")
-	return 0;
+	int choice;
+
+	while(1){
+		 printf("\n\t Algorithm Time Complexity Analysis");
+		printf("\n\t-~-~--~-~-~-~-~-~-~-~--~-~-~-~-~-~-~-\n");
+		printf("1. Bubble Sort \n");
+		printf("2. Selection Sort \n");
+		printf("3. Inserting Sort \n");
+		printf("4. Merge Sort \n");
+		printf("5. Quick Sort \n");
+		printf("6. Radix Sort \n");
+		printf("00. Exit Program \n");
+		scanf("%d",&choice);
+
+		switch(choice){
+			case 00: exit(0);
+		}
+	}
+
 }
