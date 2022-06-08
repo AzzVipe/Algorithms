a94f06cd69aa451ab90848f4ba6ed57bb1d20b5c
Azmat Ali <azmat1080@gmail.com>
2022-06-08 23:57:36 +0530

    main.c: created structure for algorithm cleaned code

--- main.c
+++ main.c
@@ -8,8 +8,32 @@
 #define QUICK_CODE		"src/.quick.txt"
 #define RADIX_CODE		"src/.radix.txt"
 
+#define BUBBLE "bubble"
+#define SELECT "select"
+#define INSERT "insert"
+#define MERGE  "merge"
+#define QUICK  "quick"
+#define RADIX  "radix"
+
 #define CLEAR_SCR        "\x1b[2J"
 #define CLEAR_SCR_HOME	 "\x1b[2J\x1b[H"
+
+struct algorithm {
+	int id;
+	char *name;
+	char *code_dest;
+	char *desc;
+};
+
+struct algorithm algos[] = {
+	{1,  BUBBLE, BUBBLE_CODE, NULL},
+	{2,  SELECT, SELECT_CODE, NULL},
+	{3,  INSERT, INSERT_CODE, NULL},
+	{4,  MERGE,  MERGE_CODE,  NULL},
+	{5,  QUICK,  QUICK_CODE,  NULL},
+	{6,  RADIX,  RADIX_CODE,  NULL},
+	{-1, NULL,   NULL,        NULL}
+};
 
 void clear()
 {
@@ -20,7 +44,7 @@ void print_menu(void)
 {
 	clear();
 	printf("\n\t Algorithm Time Complexity Analysis");
-	printf("\n\t-~-~--~-~-~-~-~-~-~-~--~-~-~-~-~-~-~-\n");
+	printf("\n\t-~-~--~-~-~-~-~-~-~-~--~-~-~-~-~-~-~-\n"); 
 	printf("1. Bubble Sort \n");
 	printf("2. Selection Sort \n");
 	printf("3. Inserting Sort \n");
@@ -29,6 +53,38 @@ void print_menu(void)
 	printf("6. Radix Sort \n");
 	printf("0. Show Menu\n");
 	printf("-1. Exit\n\n");
+}
+
+int get_algo(int num) {
+	for(int i = 0; algos[i].name; i++)
+		if(algos[i].id == num) return i;
+
+	return -1;
+}
+
+void print_algo(int algo) {
+	char *file;
+	FILE *fp;
+	int c, index;
+
+	if((index = get_algo(algo)) == -1) {
+		fprintf(stderr, "Algorithm not found!\n");
+		return;
+	}
+
+	if((fp = fopen(algos[index].code_dest, "r")) == NULL) { // opening the file
+		perror("fopen error");
+		exit(EXIT_FAILURE);
+	}
+	
+	clear(); // clearing screen with escape character sequence 
+	printf("Code for bubble sort: \n\n");
+
+	while((c = fgetc(fp)) != EOF) { // reading the file fp with fgetc, man fgetc for more detail about the function
+		printf("%c", c);
+	}
+				
+	fclose(fp); // closing the file pointer after using
 }
 	
 void main()
@@ -54,93 +110,32 @@ void main()
 
 			//Bubble Sort
 			case 1:
-				if((fp = fopen(BUBBLE_CODE, "r")) == NULL) { // opening the file
-					perror("fopen error");
-					exit(EXIT_FAILURE);
-				}
-				clear(); // clearing screen with escape character sequence 
-				printf("Code for bubble sort: \n\n");
-				while((c = fgetc(fp)) != EOF) { // reading the file fp with fgetc, man fgetc for more detail about the function
-					printf("%c", c);
-				}
-				
-				fclose(fp); // closing the file pointer after using
+				print_algo(1);
 				break;
 
 			//Selection Sort
 			case 2:
-				if((fp = fopen(SELECT_CODE, "r")) == NULL) {
-					perror("fopen error");
-					exit(EXIT_FAILURE);
-				}
-				clear();
-				printf("Code for Selection sort: \n\n");
-				while((c = fgetc(fp)) != EOF) { 
-					printf("%c", c);
-				}
-				
-				fclose(fp);
+				print_algo(2);
 				break;
 
 			//Insertion Sort
 			case 3:
-				if((fp = fopen(INSERT_CODE, "r")) == NULL) {
-					perror("fopen error");
-					exit(EXIT_FAILURE);
-				}
-				clear();
-				printf("Code for insertion sort: \n\n");
-				while((c = fgetc(fp)) != EOF) {
-					printf("%c", c);
-				}
-				
-				fclose(fp);
+				print_algo(3);
 				break;
 
 			//Merge Sort
 			case 4:
-				if((fp = fopen(MERGE_CODE, "r")) == NULL) {
-					perror("fopen error");
-					exit(EXIT_FAILURE);
-				}
-				clear();
-				printf("Code for merge sort: \n\n");
-				while((c = fgetc(fp)) != EOF) {
-					printf("%c", c);
-				}
-				
-				fclose(fp);
+				print_algo(4);
 				break;
 
 			//Quick Sort
 			case 5:
-				if((fp = fopen(QUICK_CODE, "r")) == NULL) {
-					perror("fopen error");
-					exit(EXIT_FAILURE);
-				}
-				clear();
-				printf("Code for quick sort: \n\n");
-				while((c = fgetc(fp)) != EOF) {
-					printf("%c", c);
-				}
-				
-				fclose(fp);
+				print_algo(5);
 				break;
 
 			//Radix Sort
 			case 6:
-				if((fp = fopen(RADIX_CODE, "r")) == NULL) {
-					perror("fopen error");
-					exit(EXIT_FAILURE);
-				}
-				clear();
-				printf("Code for radix sort: \n\n");
-				while((c = fgetc(fp)) != EOF) {
-					printf("%c", c);
-				}
-				
-				fclose(fp);
-				break;
+				print_algo(6);
 
 			default:
 				fprintf(stderr, "Invalid Input\n");
